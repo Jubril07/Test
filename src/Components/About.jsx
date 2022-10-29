0a9a49624879bf0014ede3c23899be5a9c4a12bc
Mubarak Jubril <mubarakjubril07@gmail.com>
2022-10-30 00:17:03 +0100

    No message

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div className="about" id="about">
       <h1>About</h1>
+      {/*This is the about section */}
       <p>
         My name is Mubarak Jubril, I am a frontend web developer and designer
         based in Nigeria. I do responsive and mobile first designs
